a6fc2165b1eeee537f595e5555914aeb1d55216c
Iryna Shenenko <163681074+Iryna-Shenenko@users.noreply.github.com>
2024-12-03 12:49:40 -0700

    add file

---
+++ src/components/App/App.jsx
@@ -0,0 +1,27 @@
+import { AppBar } from "../AppBar/AppBar";
+import { TaskForm } from "../TaskForm/TaskForm";
+import { TaskList } from "../TaskList/TaskList";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTasks } from "../../redux/operations";
+
+
+
+export default function App() {
+    const dispatch = useDispatch();
+    const isLoading = useSelector((state) => state.tasks.isLoading);
+    const error = useSelector((state) => state.tasks.error);
+
+    useEffect(() => {
+        dispatch(fetchTasks());
+    }, [dispatch]);
+    return (
+        <div>
+            <AppBar />
+            < TaskForm />
+            {isLoading && !error && <b>Request in progress ...</b>}
+            <TaskList />
+
+        </div>
+    );
+};
